5fd1d88d97d21a536f247d2beb7d225942e43787
ArthurHub <thekingarthur@gmail.com>
2015-02-03 19:36:27 +0200

    demo zoom activity - add progress bar while full zoom image is loading

--- demo/src/main/java/com/theartofdev/fastimageloaderdemo/zoom/ZoomActivity.java
+++ demo/src/main/java/com/theartofdev/fastimageloaderdemo/zoom/ZoomActivity.java
@@ -2,15 +2,9 @@ package com.theartofdev.fastimageloaderdemo.zoom;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.ProgressBar;
 
-import com.theartofdev.fastimageloader.FastImageLoader;
-import com.theartofdev.fastimageloader.LoadedFrom;
-import com.theartofdev.fastimageloader.ReusableBitmap;
-import com.theartofdev.fastimageloader.Target;
 import com.theartofdev.fastimageloaderdemo.R;
-import com.theartofdev.fastimageloaderdemo.Specs;
-
-import it.sephiroth.android.library.imagezoom.ImageViewTouch;
 
 public class ZoomActivity extends ActionBarActivity {
 
@@ -19,49 +13,9 @@ public class ZoomActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zoom);
 
+        String uri = getIntent().getStringExtra("uri");
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
         ZoomImageView zoomImageView = (ZoomImageView) findViewById(R.id.zoom_image);
-        zoomImageView.loadImage(getIntent().getStringExtra("uri"));
-        //
-        //        new setImage((ImageViewTouch) findViewById(R.id.zoom_image)).loadImage(getIntent().getStringExtra("uri"));
-    }
-
-    private class bla implements Target {
-
-        private ImageViewTouch mImageViewTouch;
-
-        private String mUrl;
-
-        private bla(ImageViewTouch imageViewTouch) {
-            mImageViewTouch = imageViewTouch;
-        }
-
-        /**
-         *
-         */
-        public void loadImage(String url) {
-            mUrl = url;
-            FastImageLoader.loadImage(this, null);
-        }
-
-        @Override
-        public String getUrl() {
-            return mUrl;
-        }
-
-        @Override
-        public String getSpecKey() {
-            return Specs.ZOOM_IMAGE;
-        }
-
-        @Override
-        public void onBitmapLoaded(ReusableBitmap bitmap, LoadedFrom from) {
-            mImageViewTouch.setImageBitmap(bitmap.getBitmap(), null, -1, 8f);
-        }
-
-        @Override
-        public void onBitmapFailed() {
-
-        }
-
+        zoomImageView.loadImage(uri, progressBar);
     }
 }
--- demo/src/main/java/com/theartofdev/fastimageloaderdemo/zoom/ZoomImageView.java
+++ demo/src/main/java/com/theartofdev/fastimageloaderdemo/zoom/ZoomImageView.java
@@ -14,7 +14,11 @@ package com.theartofdev.fastimageloaderdemo.zoom;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
+import com.theartofdev.fastimageloader.LoadedFrom;
+import com.theartofdev.fastimageloader.ReusableBitmap;
 import com.theartofdev.fastimageloader.TargetImageViewBitmapHandler;
 import com.theartofdev.fastimageloaderdemo.Specs;
 
@@ -22,27 +26,31 @@ import it.sephiroth.android.library.imagezoom.ImageViewTouch;
 
 public class ZoomImageView extends ImageViewTouch {
 
+    private ProgressBar mProgressBar;
+
     /**
      * The target image handler to load the image and control its lifecycle.
      */
-    protected TargetImageViewBitmapHandler mHandler;
+    private ZoomTargetImageViewBitmapHandler mHandler;
 
     public ZoomImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        mHandler = new TargetImageViewBitmapHandler(this);
+        mHandler = new ZoomTargetImageViewBitmapHandler(this);
         setDisplayType(DisplayType.FIT_TO_SCREEN);
     }
 
     public ZoomImageView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        mHandler = new TargetImageViewBitmapHandler(this);
+        mHandler = new ZoomTargetImageViewBitmapHandler(this);
         setDisplayType(DisplayType.FIT_TO_SCREEN);
     }
 
     /**
      * Load the given image into the zoom image view.
      */
-    public void loadImage(String url) {
+    public void loadImage(String url, ProgressBar progressBar) {
+        mProgressBar = progressBar;
+        mProgressBar.setVisibility(VISIBLE);
         mHandler.loadImage(url, Specs.ZOOM_IMAGE, Specs.IMAGE, false);
     }
 
@@ -56,6 +64,24 @@ public class ZoomImageView extends ImageViewTouch {
             mHandler.onViewShown();
         } else {
             mHandler.onViewHidden();
+        }
+    }
+
+    private final class ZoomTargetImageViewBitmapHandler extends TargetImageViewBitmapHandler {
+
+        /**
+         * @param imageView The image view to handle.
+         */
+        public ZoomTargetImageViewBitmapHandler(ImageView imageView) {
+            super(imageView);
+        }
+
+        @Override
+        protected void setImage(ReusableBitmap bitmap, LoadedFrom from) {
+            super.setImage(bitmap, from);
+            if (bitmap.getSpec().getKey().equals(Specs.ZOOM_IMAGE)) {
+                mProgressBar.setVisibility(GONE);
+            }
         }
     }
 }
